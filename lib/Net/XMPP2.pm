ba36dd453dce07a0edaf101efd44dfa437b28688
Robin Redeker <elmex@ta-sa.org>
2007-08-14 19:31:08 +0200

    release 0.07

--- lib/Net/XMPP2.pm
+++ lib/Net/XMPP2.pm
@@ -8,11 +8,11 @@ Net::XMPP2 - An implementation of the XMPP Protocol
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
@@ -116,6 +116,15 @@ Here are some notes to the last releases (release of this version is at top):
 
 =over 4
 
+=item * 0.07
+
+Many small changes in L<Net::XMPP2::Event>. Implemented XEP-0199 (XMPP Ping)
+and also whitespace pings in L<Net::XMPP2::Connection>.
+
+Also fixed some bugs.
+
+For further details look in the C<Changes> file.
+
 =item * 0.06
 
 The event API has been changed a bit, it's possible to intercept events
@@ -172,29 +181,6 @@ While implementing in band registration I implemented XEP-0066: Out of Band Data
 You can now receive and send URLs from and to others. See also L<Net::XMPP2::Ext::OOB>.
 
 I also fixed some bugs in L<Net::XMPP2::Ext::Disco>.
-
-=item * 0.03
-
-This release adds new events for attaching information to "XML" stanzas that
-are in transmission to the server. See also the events C<send_*_hook> in
-L<Net::XMPP2::Connection>.
-
-The event callbacks als don't have to return a true value anymore. What the
-return values do depends on the event now.
-
-The highlight of this release is the implementation of XEP-0114, the Jabber
-Component Protocol.
-
-It's possible to get a DOM tree from a L<Net::XMPP2::Node> now and also to
-receive the original parsed "XML" from it, which should enable full access to
-the "XML" data that was received. This also allows easy integration with other
-XML Perl modules.
-
-You can also set the initial priority of the presence in
-L<Net::XMPP2::IM::Connection> now.
-
-Please consult the Changes file for greater detail about bugfixes and new
-features.
 
 =item * older
 
