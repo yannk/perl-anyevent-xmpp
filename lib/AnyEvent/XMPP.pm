8bdc92dc52114ece6449dfaf909256350e25e6d8
Robin Redeker <elmex@ta-sa.org>
2009-02-03 10:52:38 +0100

    removed unneccessary note about TLS.

--- lib/AnyEvent/XMPP.pm
+++ lib/AnyEvent/XMPP.pm
@@ -93,10 +93,6 @@ For SASL authentication
 
 For stringprep profiles to handle JIDs.
 
-=item L<Net::SSLeay>
-
-For SSL connections.
-
 =item L<Digest::SHA1>
 
 For component authentication and old-style authentication.
@@ -197,25 +193,6 @@ L<AnyEvent::XMPP> I try to provide low level modules for speaking XMPP as define
 in RFC 3920 and RFC 3921 (see also L<AnyEvent::XMPP::Connection> and
 L<AnyEvent::XMPP::IM::Connection>). But I also try to provide a high level API for
 easier usage for instant messaging tasks and clients (eg. L<AnyEvent::XMPP::Client>).
-
-=head1 A note about TLS
-
-This module also supports TLS, as the specification of XMPP requires an
-implementation to support TLS.
-
-Maybe there are still some bugs in the handling of TLS in L<AnyEvent::XMPP::Connection>.
-So keep an eye on TLS with this module. If you encounter any problems it would be
-very helpful if you could debug them or at least send me a detailed report on how
-to reproduce the problem.
-
-(As I use this module myself I don't expect TLS to be completely broken, but it
-might break under different circumstances than I have here.  Those
-circumstances might be a different load of data pumped through the TLS
-connection.)
-
-I mainly expect problems where available data isn't properly read from the socket
-or written to it. You might want to take a look at the C<debug_send> and C<debug_recv>
-events in L<AnyEvent::XMPP::Connection>.
 
 =head1 Supported extensions
 
